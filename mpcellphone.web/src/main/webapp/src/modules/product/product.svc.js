c1b5eec1376cb9a6124a884f01ddac828bb71e92
jhrubiano10 <jh.rubiano10@uniandes.edu.co>
2015-09-02 21:32:31 -0500

    Controlador JS comentarios

--- mpcellphone.web/src/main/webapp/src/modules/product/product.svc.js
+++ mpcellphone.web/src/main/webapp/src/modules/product/product.svc.js
@@ -3,5 +3,10 @@
     
     mod.service('productService', ['CrudCreator','productContext', function(CrudCreator, context){
             CrudCreator.extendService(this, context);
+            this.saveQuestion = function(data)
+            {
+                //return this.api.one('cheapest', idBook).get();
+                console.log("llega", data);
+            };
     }]);
 })(window.angular);
