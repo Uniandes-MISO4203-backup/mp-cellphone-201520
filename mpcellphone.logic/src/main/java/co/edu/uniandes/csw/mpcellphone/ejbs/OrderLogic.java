a01b0308a07a49cee60154d5edeb2c212443f4a2
Cindy Hernández <cvho31@gmail.com>
2015-09-02 20:26:12 -0500

    Servicio OrderEntity

---
+++ mpcellphone.logic/src/main/java/co/edu/uniandes/csw/mpcellphone/ejbs/OrderLogic.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mpcellphone.ejbs;
+
+import co.edu.uniandes.csw.mpcellphone.api.IOrderLogic;
+import co.edu.uniandes.csw.mpcellphone.converters.OrderConverter;
+import co.edu.uniandes.csw.mpcellphone.dtos.OrderDTO;
+import co.edu.uniandes.csw.mpcellphone.entities.OrderEntity;
+import co.edu.uniandes.csw.mpcellphone.persistence.OrderPersistence;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ * EJB relacionado con el comportamiento de una orden
+ * @author Cindy Hernández - cv.hernandez10
+ */
+@Stateless
+public class OrderLogic implements IOrderLogic {
+ 
+    @Inject
+    private OrderPersistence persistence; 
+    
+    public int countOrder() {
+        return persistence.count();
+    }
+
+    /**
+     * Metodo encargado de obtener las órdenes de un cliente
+     * @param page
+     * @param maxRecords
+     * @return 
+     */
+    public List<OrderDTO> getOrders(Integer page, Integer maxRecords) {
+        return OrderConverter.listEntity2DTO(persistence.findAll(page, maxRecords));
+    }
+
+    /**
+     * Metodo encargado de obtener una orden solicitada a traves del id de esta
+     * @param id
+     * @return 
+     */
+    public OrderDTO getOrder(Long id) {
+        return OrderConverter.fullEntity2DTO(persistence.find(id));
+    }
+
+    /**
+     * Metodo que permite realizar la creación de una orden
+     * @param dto
+     * @return 
+     */
+    public OrderDTO createOrder(OrderDTO dto) {
+        OrderEntity entity = OrderConverter.fullDTO2Entity(dto);
+        persistence.create(entity);
+        return OrderConverter.fullEntity2DTO(entity);
+    }
+
+    /**
+     * Metodo que permite actualizar la información de una orden
+     * @param dto
+     * @return 
+     */
+    public OrderDTO updateOrder(OrderDTO dto) {
+        OrderEntity entity = persistence.update(OrderConverter.fullDTO2Entity(dto));
+        return OrderConverter.fullEntity2DTO(entity);
+    }
+
+    /**
+     * Metodo que permite eliminar una orden
+     * @param id 
+     */
+    public void deleteOrder(Long id) {
+        persistence.delete(id);
+    }
+}
---
+++ mpcellphone.web/src/main/java/co/edu/uniandes/csw/mpcellphone/services/OrderService.java
@@ -0,0 +1,96 @@
+package co.edu.uniandes.csw.mpcellphone.services;
+
+import co.edu.uniandes.csw.mpcellphone.api.IOrderLogic;
+import co.edu.uniandes.csw.mpcellphone.api.IOrderLogic;
+import co.edu.uniandes.csw.mpcellphone.dtos.OrderDTO;
+import co.edu.uniandes.csw.mpcellphone.dtos.OrderDTO;
+import co.edu.uniandes.csw.mpcellphone.providers.StatusCreated;
+import java.util.List;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Servicio REST de Order
+ * @author Cindy Hernández - cv.hernandez10
+ */
+@Path("/orders")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class OrderService {
+
+    @Inject 
+    private IOrderLogic orderLogic;
+    @Context 
+    private HttpServletResponse response;
+    
+    @QueryParam("page") private Integer page;
+    @QueryParam("maxRecords") private Integer maxRecords;
+
+    /**
+     * Metodo POST del servicio, para crear datos
+     * @param dto
+     * @return 
+     */
+    @POST
+    @StatusCreated
+    public OrderDTO createOrder(OrderDTO dto) {
+        return orderLogic.createOrder(dto);
+    }
+
+    /**
+     * Metodo GET, para obtener el listado de ordenes
+     * @return 
+     */
+    @GET
+    public List<OrderDTO> getOrders() {
+        if (page != null && maxRecords != null) {
+            this.response.setIntHeader("X-Total-Count", orderLogic.countOrder());
+        }
+        return orderLogic.getOrders(page, maxRecords);
+    }
+
+    /**
+     * Metodo GET, para obtener un dato especifico al enviar un parámetro por URL
+     * @param id
+     * @return 
+     */
+    @GET
+    @Path("{id: \\d+}")
+    public OrderDTO getOrder(@PathParam("id") Long id) {
+        return orderLogic.getOrder(id);
+    }
+
+    /**
+     * Metodo PUT, encargado de actualizar informacion de la orden
+     * @param id
+     * @param dto
+     * @return 
+     */
+    @PUT
+    @Path("{id: \\d+}")
+    public OrderDTO updateOrder(@PathParam("id") Long id, OrderDTO dto) {
+        dto.setId(id);
+        return orderLogic.updateOrder(dto);
+    }
+
+    /**
+     * Metodo DELETE, encargado de eliminar una orden
+     * @param id 
+     */
+    @DELETE
+    @Path("{id: \\d+}")
+    public void deleteOrder(@PathParam("id") Long id) {
+        orderLogic.deleteOrder(id);
+    }
+}
