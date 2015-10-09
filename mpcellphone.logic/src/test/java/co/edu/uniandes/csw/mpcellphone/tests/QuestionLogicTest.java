2677cad134323c3498bd57b7b44fea5f0410cb86
Germán González M <g.gonzalez10@uniandes.edu.co>
2015-10-08 23:12:24 -0500

    QuestionLogicTest

---
+++ mpcellphone.logic/src/test/java/co/edu/uniandes/csw/mpcellphone/tests/QuestionLogicTest.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mpcellphone.tests;
+
+import co.edu.uniandes.csw.mpcellphone.api.IQuestionLogic;
+import co.edu.uniandes.csw.mpcellphone.converters.QuestionConverter;
+import co.edu.uniandes.csw.mpcellphone.dtos.QuestionDTO;
+import co.edu.uniandes.csw.mpcellphone.ejbs.QuestionLogic;
+import co.edu.uniandes.csw.mpcellphone.entities.QuestionEntity;
+import co.edu.uniandes.csw.mpcellphone.persistence.QuestionPersistence;
+import static co.edu.uniandes.csw.mpcellphone.tests._TestUtil.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+import org.junit.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+/**
+ *
+ * @author g.gonzalez10
+ */
+@RunWith(Arquillian.class)
+public class QuestionLogicTest {
+    public static final String DEPLOY = "Prueba";
+
+    /**
+     * @return 
+     * @generated
+     */
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, DEPLOY + ".war")
+                .addPackage(QuestionEntity.class.getPackage())
+                .addPackage(QuestionDTO.class.getPackage())
+                .addPackage(QuestionConverter.class.getPackage())
+                .addPackage(QuestionLogic.class.getPackage())
+                .addPackage(IQuestionLogic.class.getPackage())
+                .addPackage(QuestionPersistence.class.getPackage())
+                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    /**
+     * @generated
+     */
+    @Inject
+    private IQuestionLogic questionLogic;
+
+    /**
+     * @generated
+     */
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * @generated
+     */
+    @Inject
+    UserTransaction utx;
+
+    /**
+     * @generated
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * @generated
+     */
+    private void clearData() {
+        em.createQuery("delete from QuestionEntity").executeUpdate();
+    }
+
+        /**
+     * @generated
+     */
+    private List<QuestionEntity> data = new ArrayList<QuestionEntity>();
+
+    /**
+     * @generated
+     */
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            QuestionEntity entity = new QuestionEntity();
+        	entity.setQuestion(generateRandom(String.class));
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+
+        /**
+     * @generated
+     */
+    @Test
+    public void createQuestionTest() {
+        QuestionDTO dto = new QuestionDTO();
+        dto.setQuestion(generateRandom(String.class));
+
+        QuestionDTO result = questionLogic.createQuestion(dto);
+
+        Assert.assertNotNull(result);
+
+        QuestionEntity entity = em.find(QuestionEntity.class, result.getId());
+
+        Assert.assertEquals(dto.getQuestion(), entity.getQuestion());
+    }
+
+    
+}
