93d085e91a9fec2edf9b21a47969b1678dffabb9
Germán González M <g.gonzalez10@uniandes.edu.co>
2015-10-12 18:27:51 -0500

    UserPersistenceTest

--- mpcellphone.logic/src/test/java/co/edu/uniandes/csw/mpcellphone/tests/UserPersistenceTest.java
+++ mpcellphone.logic/src/test/java/co/edu/uniandes/csw/mpcellphone/tests/UserPersistenceTest.java
@@ -1,10 +1,7 @@
 package co.edu.uniandes.csw.mpcellphone.tests;
 
-import co.edu.uniandes.csw.mpcellphone.entities.ProviderEntity;
 import co.edu.uniandes.csw.mpcellphone.entities.UserEntity;
-import co.edu.uniandes.csw.mpcellphone.persistence.ProviderPersistence;
 import co.edu.uniandes.csw.mpcellphone.persistence.UserPersistence;
-import static co.edu.uniandes.csw.mpcellphone.tests.ProviderPersistenceTest.DEPLOY;
 import static co.edu.uniandes.csw.mpcellphone.tests._TestUtil.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,12 +23,97 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 public class UserPersistenceTest {
-    private List<UserEntity> data = new ArrayList<UserEntity>();
     public static final String DEPLOY = "Prueba";
     
     @Inject
     private UserPersistence userPersistence;
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, DEPLOY + ".war")
+                .addPackage(UserEntity.class.getPackage())
+                .addPackage(UserPersistence.class.getPackage())
+                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    /**    /**
+     * @generated
+     */
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * @generated
+     */
+    @Inject
+    UserTransaction utx;
+
+    /**
+     * @generated
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * @generated
+     */
+    private void clearData() {
+        em.createQuery("delete from UserEntity").executeUpdate();
+    }
+
+    /**
+     * @generated
+     */
+    private List<UserEntity> data = new ArrayList<UserEntity>();
+
+    /**
+     * @generated
+     */
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            UserEntity entity = new UserEntity();
+            entity.setName(generateRandom(String.class));
+            entity.setStormpath(generateRandom(String.class));
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
     
+    /**
+     * @generated
+     */
+    @Test
+    public void createUserTest() {
+        UserEntity newEntity = new UserEntity();
+        newEntity.setName(generateRandom(String.class));
+        newEntity.setStormpath(generateRandom(String.class));
+
+        UserEntity result = userPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+
+        UserEntity entity = em.find(UserEntity.class, result.getId());
+
+        Assert.assertEquals(newEntity.getName(), entity.getName());
+        Assert.assertEquals(newEntity.getStormpath(), entity.getStormpath());
+    }
+
+    /*
     @Test
     public void getUserTest() {
         UserEntity entity = data.get(0);
@@ -40,26 +122,6 @@ public class UserPersistenceTest {
         Assert.assertEquals(entity.getName(), newEntity.getName());
         Assert.assertEquals(entity.getId(), newEntity.getId());
     }
-    
-    @Deployment
-    public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class, DEPLOY + ".war")
-                .addPackage(ProviderEntity.class.getPackage())
-                .addPackage(ProviderPersistence.class.getPackage())
-                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
-                .addAsWebInfResource("META-INF/beans.xml", "beans.xml");
-    }
-/*
-        public UserEntity getUsers(String userId){
-        try {
-            return this.executeSingleNamedQuery("User.FindAll");
-        } catch (NoResultException e) {
-            return null;
-        }
-    }
     */
-    /**
-     * @generated
-     */
     
 }
