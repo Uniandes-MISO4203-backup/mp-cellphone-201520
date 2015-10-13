7521f876d3b8eaa00a9df4c28699952b17b874b3
César Forero <cesar.forero.gz@gmail.com>
2015-10-13 10:38:49 -0500

    Merge with development

--- mpcellphone.web/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ mpcellphone.web/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -39,6 +39,7 @@ public class ApplicationConfig extends Application {
         resources.add(co.edu.uniandes.csw.mpcellphone.services.PaymentMethodService.class);
         resources.add(co.edu.uniandes.csw.mpcellphone.services.ProductService.class);
         resources.add(co.edu.uniandes.csw.mpcellphone.services.ProviderService.class);
+        resources.add(co.edu.uniandes.csw.mpcellphone.services.SaleService.class);
         resources.add(co.edu.uniandes.csw.mpcellphone.services.ShipService.class);
         resources.add(co.edu.uniandes.csw.mpcellphone.services.ShippingTypeService.class);
         resources.add(co.edu.uniandes.csw.mpcellphone.services.TaxService.class);
