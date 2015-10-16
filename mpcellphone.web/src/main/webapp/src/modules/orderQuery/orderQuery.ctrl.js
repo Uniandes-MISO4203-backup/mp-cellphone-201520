a50f8c9d20cee7cabc0154b64848cdcef0a9b6f7
Germán González M <g.gonzalez10@uniandes.edu.co>
2015-10-16 08:43:43 -0500

    Ajustes orderQuery

--- mpcellphone.web/src/main/webapp/src/modules/orderQuery/orderQuery.ctrl.js
+++ mpcellphone.web/src/main/webapp/src/modules/orderQuery/orderQuery.ctrl.js
@@ -4,6 +4,16 @@
             CrudCreator.extendController(this, svc, $scope, model, 'orderQuery', 'OrderQuery');
             if (authSvc.getCurrentUser()){
                 var self = this;
+                var ocultaCampos = function (tipo)
+                {
+                    for (var i = 1; i <= model.fields.length; i++)
+                    {
+                        if (!model.fields[i - 1].visible)
+                        {
+                            $(tipo + ":nth-child(" + i + ")").css({"display": "none"});
+                        }
+                    }
+                };
                 svc.getRoleOQ().then(function (data){
                     switch (data.role){
                         //Para mostrar la vista de administrador...
@@ -21,6 +31,25 @@
                             model.fields("clientName").visible=false;
                             model.fields("providerName").visible=true;
                             ocultaCampos("th");
+                            var listSaleByClient = function (record){
+                                svc.getSaleByClient(record.clientId).then(function (client) {
+                                    $scope.records = [];
+                                    $scope.records.push(client);
+                                });
+                            };
+                            this.saleByClientActions = [{
+                                    name: 'saleByClient',
+                                    displayName: 'Sale By Client',
+                                    icon: 'list-alt',
+                                    class: 'info',
+                                    fn: function (record) {
+                                        listSaleByClient(record);
+                                    },
+                                    show: function () {
+                                        return true;
+                                    }
+                                }
+                            ]
                             self.fetchRecords().then(function () {
                                 $scope.$watch(function () {
                                     ocultaCampos("td");
@@ -31,12 +60,25 @@
                             model.fields("clientName").visible=true;
                             model.fields("providerName").visible=false;
                             ocultaCampos("th");
-                            var listSaleByClient = function (record){
-                                svc.getSaleByClient(record.providerId).then(function (provider) {
+                            var listSaleByProvider = function (record){
+                                svc.getSaleByProvider(record.providerId).then(function (provider) {
                                     $scope.records = [];
                                     $scope.records.push(provider);
                                 });
                             };
+                            this.saleBProviderActions = [{
+                                    name: 'saleByProvider',
+                                    displayName: 'Sale By Provider',
+                                    icon: 'list-alt',
+                                    class: 'info',
+                                    fn: function (record) {
+                                        listSaleByProvider(record);
+                                    },
+                                    show: function () {
+                                        return true;
+                                    }
+                                }
+                            ]
                             self.fetchRecords().then(function () {
                                 $scope.$watch(function () {
                                     ocultaCampos("td");
@@ -53,16 +95,6 @@
             {
                 $location.path('/login');
             }
-            var ocultaCampos = function (tipo)
-            {
-                for (var i = 1; i <= model.fields.length; i++)
-                {
-                    if (!model.fields[i - 1].visible)
-                    {
-                        $(tipo + ":nth-child(" + i + ")").css({"display": "none"});
-                    }
-                }
-            };
         }]);
     mod.controller('shoppingCartCtrl', ['CrudCreator', '$scope', 'cartItemModel', function (CrudCreator, $scope, model) {
             CrudCreator.extendCompChildCtrl(this, $scope, model, 'shoppingCart', 'client');
