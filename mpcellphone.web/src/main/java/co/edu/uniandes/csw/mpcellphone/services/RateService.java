f96b249cdd7ac5943851a28e9a0dc1c4875d22ed
Cindy Hernández <cvho31@gmail.com>
2015-11-10 12:34:43 -0500

    Mejora Deuda Técnica

--- mpcellphone.web/src/main/java/co/edu/uniandes/csw/mpcellphone/services/RateService.java
+++ mpcellphone.web/src/main/java/co/edu/uniandes/csw/mpcellphone/services/RateService.java
@@ -6,12 +6,10 @@ import co.edu.uniandes.csw.mpcellphone.dtos.ClientDTO;
 import co.edu.uniandes.csw.mpcellphone.dtos.RateProductDTO;
 import co.edu.uniandes.csw.mpcellphone.dtos.RateProviderDTO;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import org.apache.shiro.SecurityUtils;
 
