7f5afcee8d49aaf62befb6369e90432d1d0ac5fe
Mauricio Amaya Rios <mauro014@gmail.com>
2015-11-10 09:22:52 -0500

    MOD: Corrección de velocidad de slider

--- mpcellphone.logic/src/main/java/co/edu/uniandes/csw/mpcellphone/entities/PhotoEntity.java
+++ mpcellphone.logic/src/main/java/co/edu/uniandes/csw/mpcellphone/entities/PhotoEntity.java
@@ -6,6 +6,7 @@
 package co.edu.uniandes.csw.mpcellphone.entities;
 
 import java.io.Serializable;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
